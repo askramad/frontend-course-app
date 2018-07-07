93d58516f3023262ba087a91a512943acbad3243
askr med <askr.amad@gmail.com>
2018-07-07 16:28:38 +0000

    frontend course app

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {RouterModule ,Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
+import { CreateComponent } from './components/create/create.component';
+
+const appRoutes:Routes =[
+  {path:'create', component:CreateComponent},
+  {path:'edit/:id', component:EditComponent},
+  {path:'list', component:ListComponent},
+  {path :'', redirectTo:'list', pathMatch:'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
