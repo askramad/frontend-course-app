799c0c3f132e13d102cdad3732efbc137cbfe6ce
askr med <askr.amad@gmail.com>
2018-07-16 21:11:20 +0000

    add service issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 import {RouterModule ,Routes} from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
+import {HttpClientModule} from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { ListComponent } from './components/list/list.component';
 import { EditComponent } from './components/edit/edit.component';
 import { CreateComponent } from './components/create/create.component';
+
+import {IssueService} from './issue.service';
 
 const appRoutes:Routes =[
   {path:'create', component:CreateComponent},
@@ -27,9 +31,10 @@ const appRoutes:Routes =[
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [],
+  providers: [IssueService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
